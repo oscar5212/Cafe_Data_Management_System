1c1136ab9accc595543571ca40f8d975d1ba2455
Oscar Choy <oscar.choy.oc@gmail.com>
2023-10-11 22:55:18 +0100

    Normal week2 update of mini project

--- cafe_e2back.py
+++ cafe_e2back.py
@@ -2,39 +2,43 @@ products = ['Bacon Roll', 'Banana', 'Apple',
             'Ham & Chess Tosties', 'Chocolate',
             'Latte', 'Cappuccino', 'Chia Latte',
             'Macchiato'] # stores items for sales
-order = {'Customer_name':'Ben Conor', 'Customer_address':'077438379', 'Customer_phone': 'Unit 2, 12 Main Street, LONDON, WH1 2ER', 'Status': 'Preparaing'}
+order = {'Customer_name':'Ben Conor',
+         'Customer_address':'077438379',
+         'Customer_phone': 'Unit 2, 12 Main Street, LONDON, WH1 2ER',
+         'Status': 'Preparaing'}
 order_list = []
 status_list = []
-products_manu=("""
-      Baking Cafe Product Manu
+products_menu=("""
+      Baking Cafe Product Menu
     ===========================
     1. Products
     2. Add New Products
     3. Update Products
     4. Delete Products
-    0. Exit""")
-orders_manu = ("""
-        Baking Cafe Orders Manu
+    0. back to Main Menu""")
+orders_menu = ("""
+        Baking Cafe Orders Menu
         ======================
         1. Order Record
         2. Start New Orders
         3. Update Orders Status
         4. Update Orders Details
-        5. Delete Orders""")
+        5. Delete Orders
+        0. Back to Main Menu""")
 keep_looping = True
 while keep_looping == True: # main manu
   print("""
-        Baking Cafe Main Manu
+        Baking Cafe Main Menu
         ======================
-        1. Products Manu
-        2. Orders Manu
+        1. Products Menu
+        2. Orders Menu
         0. Exit""")
-  manu_option = input('Which manu?')
-  if manu_option == '0':
+  menu_option = input('Which menu?')
+  if menu_option == '0':
     break
-  elif manu_option == '1':              # products manu
+  elif menu_option == '1':              # products manu
     while keep_looping == True:
-      print(products_manu)
+      print(products_menu)
       product_choice = input('What are you going to do? ')
 
       if product_choice == '1':   # identifies Products in 1
@@ -73,12 +77,12 @@ while keep_looping == True: # main manu
       elif product_choice == '0': # exit product manu
           break
 
-  elif manu_option == '2':  # enter orders manu
+  elif menu_option == '2':  # enter orders manu
         while keep_looping == True:
-            print(orders_manu)
+            print(orders_menu)
             order_choice = input('Any orders? ')
             if order_choice == '1':    # show orders
-                print(str(order_list).replace("{", " ").replace("}", " ").replace(",", "\n").replace(('"'), (' ')).replace(("'"), (" ")))
+                print(str(order_list).replace("{", " ").replace("}", " ").replace(",", "\n").replace('"', ' ').replace("'", " ").replace("[", "\n").replace("]", "\n********"))
 
             elif order_choice == '2': # add order
                 name =  input ("Waht's your name? ")
@@ -91,59 +95,56 @@ while keep_looping == True: # main manu
                 status_list.append('preparaing')
                 for i in range(1):
                   order_list.append(order.copy())
-                print(str(order_list).replace("{", " ").replace("}", " ").replace(",", "\n").replace(('"'), (' ')).replace(("'"), (" ")))
+                print("New Order\n" + str(order_list).replace("{", " ").replace("}", " ").replace(",", "\n").replace('"', ' ').replace("'", " ").replace("[", "\n").replace("]", "\n********"))
 
-            elif order_choice == '3':  # edit status of order
-                order_list = ["Order Number: {} :{}".format(o, orders) for o, orders in enumerate(order_list)]
-                print(str(order_list).replace("{", " ").replace("}", " ").replace(",", "\n").replace(('"'), (' ')).replace(("'"), (" ")))
-                edit_status_in = int(input ('Which one to be edited? '))
-                if edit_status_in < len(order_list):
-                  print("1. Preparing")
-                  print("2. Ready to Collect / Deliver")
-                  print("3. Delivered / Collected")
-                  edit_status = int(input ('Please update the status! '))
-                  if edit_status == 1:
-                     order_list[edit_status_in]["Status"] = "Preparing"
-                  elif edit_status == 2:
-                     order_list[edit_status_in]["Status"] = "Ready to Collect / Deliver"
-                  elif edit_status == 3:
-                     order_list[edit_status_in]["Status"] = "Delivered / Collected"
-                     print(str(order_list).replace("{", " ").replace("}", " ").replace(",", "\n").replace('"'), (' '). replace("'"), (" "))
-                  else:
-                    print("Please Try again")
-                else:
-                  print("Please Enter Correct Index!")
 
-                status_list = ['Order Number: {}\n {} \n'.format(s, status) for s, status in enumerate(status_list)]
-                print(str(status_list).replace("{", " ").replace("}", " ").replace(",", "\n").replace('"', ' ').replace("'", " "))
-                status_index = int(input("Please enter the index! "))
-                change_status = input("Please enter new status!")
-                if status_index < len(status_list):
-                    status_list[status_index]= change_status
-                    print(str(status_list).replace("{", " ").replace("}", " ").replace(",", "\n").replace('"', ' ').replace("'", " "))
-                elif status_index> len(status_list):
-                    print("Please enter a correct index")
+            elif order_choice == "3":
+                print("Orders:")
+                for index, order in enumerate(order_list):
+                    print("Order ID :", index, "\n", str(order).replace("{", " ").replace("}", " ").replace(",", "\n").replace('"', ' ').replace("'", " "),"\n********")
+                    order_index = int(input("Enter order index to update status: "))
+                    if order_index < len(order_list):
+                        print("1. Preparing")
+                        print("2. Ready to Deliver / Collect")
+                        print("3. Delivered / Collected")
+                        status_choice = int(input("Enter status option: "))
+                        if status_choice == 1:
+                            order_list[order_index]["Status"] = "Preparing"
+                        elif status_choice == 2:
+                            order_list[order_index]["Status"] = "Ready to Deliver / Collect"
+                        elif status_choice == 3:
+                            order_list[order_index]["Status"] = "Delivered / Collected"
+                        else:
+                            print("Invalid status option. Please try again.")
+                            print("Amended Order\n" + str(order_list).replace("{", " ").replace("}", " ").replace(",", "\n").replace('"', ' ').replace("'", " ").replace("[", "\n").replace("]", "\n********"))
+                    else:
+                        print("Invalid status option. Please try again.")
 
-            elif order_choice == '4': # edit details of order
-                order_list = ['{}: {}'.format(o, orders) for o, orders in enumerate(order_list)]
-                print(str(order_list).replace("{", " ").replace("}", " ").replace(",", "\n").replace('"', ' ').replace("'", " "))
-                edit_order_in = int(input ('Please enter an index value! '))
-                for key in order_list[edit_order_in]:
-                  edit_option = input (f"Enter new {key}: ")
-                  if edit_option != '':
-                    order_list[edit_order_in][key] = edit_option
-                    print(str(order_list).replace("{", " ").replace("}", " ").replace(",", "\n").replace('"', ' ').replace("'", " "))
-                else:
-                  print("Please Enter Correct Instruction Again! ")
+            elif order_choice == "4":
+                print("Orders:")
+                for index, order in enumerate(order_list):
+                    print("Order ID :", index, "\n", str(order).replace("{", " ").replace("}", " ").replace(",", "\n").replace('"', ' ').replace("'", " "),"\n********")
+                    order_index = input("Enter order index to update: ")
+                    order_index = int(order_index)
+                    if order_index < len(order_list):
+                        for key in order_list[order_index]:
+                            new_value = input(f"Enter new {key} (press enter to keep current value): ")
+                            if new_value != "":
+                                order_list[order_index][key] = new_value
+                                print("Order updated successfully.")
+                                print("Updated Order\n" + str(order_list).replace("{", " ").replace("}", " ").replace(",", "\n").replace('"', ' ').replace("'", " ").replace("[", "\n").replace("]", "\n********"))
+                    else:
+                        print("Invalid input or index. Please try again.")
 
             elif order_choice == '5': # delete order
                 order_list = ['{}: {}'.format(o, orders) for o, orders in enumerate(order_list)]
-                print(str(order_list).replace("{", " ").replace("}", " ").replace(",", "\n").replace('"', ' ').replace("'", " "))
+                print(str(order_list).replace("{", " ").replace("}", " ").replace(",", "\n").replace('"', ' ').replace("'", " ").replace("[", "\n").replace("]", "\n********"))
                 delete_order_in = input('Please enter an index value to delete order! ')
                 delete_order_in = int(delete_order_in)
                 if delete_order_in < len(order_list):
                     del order_list[delete_order_in]
                 if delete_order_in >= len(order_list):
                     print("Please entre the correct index! ")
-  else:                       # Exit of main manu
-      break
+
+            elif order_choice == '0':
+                break

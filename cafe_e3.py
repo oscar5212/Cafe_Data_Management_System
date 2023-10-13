614c01f81e851f32c631825e901bb2766a64bf63
Oscar Choy <oscar.choy.oc@gmail.com>
2023-10-13 11:28:02 +0100

    Products, Couriers and Orders Funciton Combined

--- cafe_e3.py
+++ cafe_e3.py
@@ -22,9 +22,19 @@ couriers_menu =("""
     3. Update Couriers
     4. Delete Couriers
     0. Back to Main Menu""") # Layout of Couriers Menu
+orders_menu = ("""
+        Baking Cafe Orders Menu
+        ======================
+        1. Order Record
+        2. Start New Orders
+        3. Update Orders Status
+        4. Update Orders Details
+        5. Delete Orders
+        0. Back to Main Menu""") # Layout of Orders Menu
 order = {'Customer_name':'Ben Conor',
          'Customer_address':'077438379',
          'Customer_phone': 'Unit 2, 12 Main Street, LONDON, WH1 2ER',
+         'Courier' : 2,
          'Status': 'Preparaing'}  # dictionary of order
 order_list = [] # lsit of orders
 status_list = [] # list of order status
@@ -100,12 +110,12 @@ while keep_looping == True: # main manu
             break
 
           elif couriers_choice == '1': # print couriers list
-                print(f"Couriers List\n{couriers_list}")
+                print(f"Couriers List\n{couriers_list}".lstrip())
 
           elif couriers_choice == '2':  # add new couriers
             courier_name = input("Please Enter Couriers Name! ")
             couriers_list.append(courier_name)
-            print(f"New Couriers List\n{couriers_list}")
+            print(f"New Couriers List\n{couriers_list}".lstrip())
 
           elif couriers_choice == '3': # update existed couriers
             ori_couriers_list = ['{}: {}'.format(c, courier) for c, courier in enumerate(couriers_list)]
@@ -114,7 +124,7 @@ while keep_looping == True: # main manu
             new_name = input("Please Type New Courier Name ! ")
             if courier_index < len(ori_couriers_list):
               couriers_list[courier_index] = new_name
-              print(f"Updated Couriers List\n{couriers_list}")
+              print(f"Updated Couriers List\n{couriers_list}".lstrip())
             else:
               print("Please Enter Corect Index! ")
 
@@ -124,6 +134,98 @@ while keep_looping == True: # main manu
             courier_index = int(input("Who need to be deleted? Please Enter Index! "))
             if courier_index < len(ori_couriers_list):
               del couriers_list[courier_index]
-              print(f"Reduced Couriers List\n{couriers_list}")
+              print(f"Reduced Couriers List\n{couriers_list}".lstrip())
             else:
               print("Please Enter Correct Index Value! ")
+
+  elif menu_option == '3':  # enter orders manu
+        while keep_looping == True:
+            print(orders_menu)
+            order_choice = input('Any orders? ')
+            if order_choice == '1':    # show orders
+                print(str(order_list).replace("{", " ").replace("}", " ").replace(",", "\n").replace('"', ' ').replace("'", " ").replace("[", "\n").replace("]", "\n********").lstrip())
+
+            elif order_choice == '2': # add order
+                name =  input ("Waht's your name? ")
+                address = input ("What's your address? ")
+                phone = input ("What's your phone number? ")
+                ori_couriers_list = ['Available Couriers {} : {}'.format(c, courier) for c, courier in enumerate(couriers_list)]
+                print(ori_couriers_list)
+                courier = int(input ("Who deliver the order? Please enter the index"))
+                order['Customer_name'] = name
+                order['Customer_address'] = address
+                order['Customer_phone'] = phone
+                order['Courier'] = courier
+                order['Status'] = 'preparaing'
+                status_list.append('preparaing')
+                for i in range(1):
+                  order_list.append(order.copy())
+                print("New Order\n" + str(order_list).replace("{", " ").replace("}", " ").replace(",", "\n").replace('"', ' ').replace("'", " ").replace("[", "\n").replace("]", "\n********").lstrip())
+
+
+            elif order_choice == "3":
+                print("Orders:")
+                for index, order in enumerate(order_list):
+                    print("Order ID :", index, "\n", str(order).replace("{", " ").replace("}", " ").replace(",", "\n").replace('"', ' ').replace("'", " "),"\n********")
+                    order_index = int(input("Enter order index to update status: "))
+                    if order_index < len(order_list):
+                        print("1. Preparing")
+                        print("2. Ready to Deliver / Collect")
+                        print("3. Delivered / Collected")
+                        status_choice = int(input("Enter status option: "))
+                        if status_choice == 1:
+                            order_list[order_index]["Status"] = "Preparing"
+                        elif status_choice == 2:
+                            order_list[order_index]["Status"] = "Ready to Deliver / Collect"
+                        elif status_choice == 3:
+                            order_list[order_index]["Status"] = "Delivered / Collected"
+                        else:
+                            print("Invalid status option. Please try again.")
+                            print("Amended Order\n" + str(order_list).replace("{", " ").replace("}", " ").replace(",", "\n").replace('"', ' ').replace("'", " ").replace("[", "\n").replace("]", "\n********"))
+                    else:
+                        print("Invalid status option. Please try again.")
+
+                for index, status in enumerate(status_list): # update of status list
+                  print("Order ID :", index, "\n", str(status).replace("{", " ").replace("}", " ").replace(",", "\n").replace('"', ' ').replace("'", " "),"\n********")
+                  status_index = int(input("Enter status index to update status list: "))
+                  if status_index < len(status_list):
+                        print("1. Preparing")
+                        print("2. Ready to Deliver / Collect")
+                        print("3. Delivered / Collected")
+                        status_choice = int(input("Enter status option: "))
+                        if status_choice == 1:
+                            status_list[status_index] = "Preparing"
+                        elif status_choice == 2:
+                            status_list[status_index] = "Ready to Deliver / Collect"
+                        elif status_choice == 3:
+                            status_list[status_index] = "Delivered / Collected"
+
+
+            elif order_choice == "4":
+                print("Orders:")
+                for index, order in enumerate(order_list):
+                    print("Order ID :", index, "\n", str(order).replace("{", " ").replace("}", " ").replace(",", "\n").replace('"', ' ').replace("'", " "),"\n********")
+                    order_index = input("Enter order index to update: ")
+                    order_index = int(order_index)
+                    if order_index < len(order_list):
+                        for key in order_list[order_index]:
+                            new_value = input(f"Enter new {key} (press enter to keep current value): ")
+                            if new_value != "":
+                                order_list[order_index][key] = new_value
+                                print("Order updated successfully.")
+                                print("Updated Order\n" + str(order_list).replace("{", " ").replace("}", " ").replace(",", "\n").replace('"', ' ').replace("'", " ").replace("[", "\n").replace("]", "\n********"))
+                    else:
+                        print("Invalid input or index. Please try again.")
+
+            elif order_choice == '5': # delete order
+                order_list = ['{}: {}'.format(o, orders) for o, orders in enumerate(order_list)]
+                print(str(order_list).replace("{", " ").replace("}", " ").replace(",", "\n").replace('"', ' ').replace("'", " ").replace("[", "\n").replace("]", "\n********"))
+                delete_order_in = input('Please enter an index value to delete order! ')
+                delete_order_in = int(delete_order_in)
+                if delete_order_in < len(order_list):
+                    del order_list[delete_order_in]
+                if delete_order_in >= len(order_list):
+                    print("Please entre the correct index! ")
+
+            elif order_choice == '0':
+                break

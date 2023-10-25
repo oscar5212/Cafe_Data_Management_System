450535653b9cb601a610d970613715de0431361a
Oscar Choy <oscar.choy.oc@gmail.com>
2023-10-25 14:23:53 +0100

    Week 6 component

--- db_orders.py
+++ db_orders.py
@@ -11,20 +11,20 @@ user_password = os.environ.get("mysql_pass")
 
 while True:
     def db_connection():
-        print('Opening connection...')
+        print('Opening connection...')  # establish connection to mysql
         conn = pymysql.connect(
         host=host_name,
         database=database_name,
         user=user_name,
         password=user_password)
         cur = conn.cursor()
-        orders_option = input("Please Enter a choice! ")
+        orders_option = input("Please Enter a Choice! ")
         print(orders_menu)
         if orders_option == "0":         # back to main menu
             conn.close()
             break
         elif orders_option == "1":       # list all orders
-            cur.execute("select * from orders")
+            cur.execute("SELECT * FROM orders")
             orders_list = cur.fetchall()
             for order in orders_list:
                 print(order)
@@ -34,19 +34,19 @@ while True:
             customer_name = input("Enter Customer's Name! ")
             customer_address = input ("Enter Customer Address! ")
             customer_phone = input("Enter Customer Phone Number! ")
-            cur.execute("select * from products")
+            cur.execute("SELECT * FROM products")
             products_list = cur.fetchall()
             for product in products_list:
                 print(product)
-            items = input("Entre Products' ID! (seperate with comma or more than 1 items)")
-            cur.execute("select * from couriers")
+            items = input("Entre Products' ID! (seperate with comma for more than 1 item)")
+            cur.execute("SELECT * FROM couriers")
             couriers_list = cur.fetchall()
             for courier in couriers_list:
                 print(courier)
             couriers = int(input("Select Couriers by their ID! "))
             status = 1
             try:
-                cur.execute("INSERT INTO orders (customer_name, customer_address, customer_phone, couriers, status, items ) VALUES (%s, %s, %s, %s, %s, %s)", (customer_name, customer_address, customer_phone, couriers, status, items))
+                cur.execute("INSERT INTO orders (customer_name, customer_address, customer_phone, couriers, order_status_id, items ) VALUES (%s, %s, %s, %s, %s, %s)", (customer_name, customer_address, customer_phone, couriers, status, items))
                 conn.commit()
                 orders_list = cur.fetchall()
                 for order in orders_list:
@@ -56,12 +56,12 @@ while True:
                 print("Error Found:", e)
 
         elif orders_option == "3":    # update order status
-            cur.execute("select * from orders")
+            cur.execute("SELECT * FROM orders")
             orders_list = cur.fetchall()
             for order in orders_list:
                 print(order)
-            order_id = int(input("Enter ID of Couriers!"))
-            cur.execute("select * from orders_status")
+            order_id = int(input("Enter ID of Orders!"))
+            cur.execute("SELECT * FROM orders_status")
             orders_status_list = cur.fetchall()
             for order_status in orders_status_list:
                 print(order_status)
@@ -75,7 +75,6 @@ while True:
                     print(status)
             except Exception as e:
                 print("Error Found:", e)
-
         elif orders_option == "4":   # update orders
             cur.execute("SELECT * FROM orders")
             orders_list = cur.fetchall()
@@ -98,10 +97,8 @@ while True:
             if order_id != "":
                 cur.execute("UPDATE orders SET (customer_name, customer_address, customer_phone, couriers, items ) VALUES (%s, %s, %s, %s, %s)", (customer_name, customer_address, customer_phone, couriers, items))
                 conn.commit()
-                
-
-
-
+            elif order_id == "":
+                pass
         elif orders_option == "5":   # delete product from product list
             cur.execute("select * from orders")
             orders_list = cur.fetchall()
